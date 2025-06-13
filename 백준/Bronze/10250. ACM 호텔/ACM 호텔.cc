093f2f6d279e904e00c1640a6b1264789fd62174
Daesub <eotjq99@gmail.com>
2025-06-13 14:35:04 +0900

    [Bronze III] Title: ACM 호텔, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int t, h, w, n;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++) {
+        scanf("%d %d %d", &h, &w, &n);
+        if (n % h == 0) {
+            printf("%d\n", h * 100 + (n / h));
+        }
+        else {
+            printf("%d\n", ((n % h) * 100) + ((n / h) + 1));
+        }
+    }
+    return 0;
+}
