6aa7bf65bd30351e4215652969a065a2d72cc5fc
Daesub <eotjq99@gmail.com>
2025-06-15 23:44:54 +0900

    [Silver I] Title: 계단, Time: 196 ms, Memory: 138260 KB, Score: 150 point -BaekjoonHub

--- 백준/Silver/21600. 계단/계단.py
+++ 백준/Silver/21600. 계단/계단.py
@@ -3,19 +3,22 @@ input = sys.stdin.readline
 
 n = int(input())
 arr = list(map(int, input().split()))
-left = 1
-left_max = 1
+
+if len(set(arr)) == 1:
+    print(min(n, arr[0]))
+    sys.exit()
+stair = 1
+stair_max = 1
 
 i = 1
 while i < n:
-    if arr[i] > left:
-        left += 1
-        left_max = max(left_max, left)
+    if arr[i] > stair:
+        stair += 1
+        stair_max = max(stair_max, stair)
     else:
         i -= (arr[i] - 1)
         if i <= 0:
             i = 1
-        left_max = max(left_max, left)
-        left = 1
+        stair = 1
     i += 1
-print(left_max)
+print(stair_max)
