c7f7d5555a5b41d91751ef7f3cd4323c03b377c5
Daesub <eotjq99@gmail.com>
2025-06-28 22:08:20 +0900

    [Bronze II] Title: 거스름돈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -0,0 +1,22 @@
+n = 1000 - int(input())
+cnt = 0
+while n != 0:
+    if n >= 500:
+        n -= 500
+        cnt += 1
+    elif n >= 100:
+        n -= 100
+        cnt += 1
+    elif n >= 50:
+        n -= 50
+        cnt += 1
+    elif n >= 10:
+        n -= 10
+        cnt += 1
+    elif n >= 5:
+        n -= 5
+        cnt += 1
+    elif n >= 1:
+        n -= 1
+        cnt += 1
+print(cnt)
