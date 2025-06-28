3fc018421ec787a1fd978215329b9bf6073e3c0b
Daesub <eotjq99@gmail.com>
2025-06-28 22:45:37 +0900

    [Bronze III] Title: 배수 찾기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4504. 배수 찾기/배수 찾기.py
@@ -0,0 +1,9 @@
+n = int(input())
+while True:
+    k = int(input())
+    if k == 0:
+        break
+    if k % n == 0:
+        print(f"{k} is a multiple of {n}.")
+    else:
+        print(f"{k} is NOT a multiple of {n}.")
